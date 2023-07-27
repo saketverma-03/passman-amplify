91d23ddbd6d62a69c2066841b374946a3d59e0d3
saket verma <saketverma831@gmail.com>
2023-07-27 11:01:32 +0530

    Added: Password Generator Function

---
+++ src/server/util.js
@@ -0,0 +1,14 @@
+export const isOffline = () => {
+    return !navigator.onLine
+}
+
+export const generateStrongPassword = () => {
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const passwordLength = 12;
+    let password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber +1);
+       }
+    return password;
+}
