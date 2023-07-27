e5257d4b34b8c61547dc456889a90e76e541d1a3
saket verma <saketverma831@gmail.com>
2023-07-27 11:01:09 +0530

    Added API requests for app sync

---
+++ src/server/user.js
@@ -0,0 +1,63 @@
+import { API as amplifApi } from "aws-amplify";
+
+/* List */
+export async function getAllPasswords(userId) {
+  const listAllPasswordForuser = (userId) => `query MyQuery {
+    listPasswords(filter: {userId: {eq: "${userId}"},_deleted: {ne: true}}) {
+      items {
+        userId
+        id
+        website
+        password
+        _version
+        username
+      }
+    }
+  }
+  `;
+  const list = await amplifApi.graphql({
+    query: listAllPasswordForuser(userId),
+  });
+  return list;
+}
+
+/* Create */
+export async function createServiceCredentials(newCredentials) {
+  const { product, userId, password, username } = newCredentials;
+  const res = await amplifApi.graphql({
+    query: `mutation MyMutation {
+    createPasswords(
+      input: {username: "${username}", password: "${password}", userId: "${userId}", website: "${product}"}
+    ) {
+      id
+    }
+  }
+  `,
+  });
+  return res;
+}
+
+/* Update */
+export async function updateCredentials(values) {
+  const { id, username, userId, password, _version, website } = values;
+  const mutationUpdateCredentials = `mutation MyMutation {
+  updatePasswords(
+    input: {id: "${id}", password: "${password}", username: "${username}", website: "${website}", _version: ${_version}}
+  ) {
+    id
+  }
+}`;
+  return await amplifApi.graphql({ query: mutationUpdateCredentials });
+}
+
+/* Delete */
+export async function mutationDeleteCredential(id, _version) {
+  const query = `mutation MyMutation {
+      deletePasswords(
+        input: {id: "${id}", _version: ${_version} }
+      ) {
+        _deleted
+      }
+    }`;
+  return await amplifApi.graphql({ query: query });
+}
